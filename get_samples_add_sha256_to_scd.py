9e4e607066d6a4dc15e86438c2211aba073a8edc
Michael Auger <miauger@cisco.com>
2019-02-21 21:23:57 -0500

    Code cleanup
    Change AMP credentials to align with naming convention
    Remove redundant auth parameter from AMP POST request
    Added an output based on success of failure

--- get_samples_add_sha256_to_scd.py
+++ get_samples_add_sha256_to_scd.py
@@ -4,7 +4,7 @@ import requests
 tg_api_key = 'asdf1234asdf1234asdf1234'
 
 # AMP for Endpoint API Credentials
-client_id = 'a1b2c3d4e5f6g7h8i9j0'
+amp_client_id = 'a1b2c3d4e5f6g7h8i9j0'
 amp_api_key = 'a1b2c3d4-e5f6-g7h8-i9j0-k1l2m3n4o5p6'
 
 # Simple custom detection GUID that SHA256s will be added to
@@ -12,7 +12,7 @@ scd_guid = 'asdf1234-asdf-1234-asdf-12344asdf123'
 
 # Instatiate a session to be used for the AMP for Endpoints API
 amp_session = requests.session()
-amp_session.auth = (client_id, amp_api_key)
+amp_session.auth = (amp_client_id, amp_api_key)
 
 # Threat Grid URL used for collecting samples
 tg_url = 'https://panacea.threatgrid.com/api/v2/search/submissions'
@@ -24,10 +24,10 @@ tg_parameters = {'api_key': tg_api_key,
                  'q':'analysis.threat_score:>=95'}
 
 # Query Threat Grid for samples
-response = requests.get(tg_url, params=tg_parameters)
+request = requests.get(tg_url, params=tg_parameters)
 
 # Decode the JSON response
-json_response = response.json()
+json_response = request.json()
 
 # Name the 'items' list in the JSON response
 items = json_response['data']['items']
@@ -38,10 +38,16 @@ for sample in items:
     sha256 = sample['item']['sha256']
 
     # Print the SHA256 about to be added to the Simple Custom Detectoin list
-    print(sha256)
+    print(sha256, end=' '),
 
     # Format the URL to add the SHA256 to the Simple Custom Detectoin list
     add_sha256_url = 'https://api.amp.cisco.com/v1/file_lists/{}/files/{}'.format(scd_guid, sha256)
 
     # Add the SHA256 to the Simple Custom Detection
-    response = amp_session.post(add_sha256_url, auth=(client_id, amp_api_key))
+    amp_request = amp_session.post(add_sha256_url)
+
+    # Check the returned status code and print if it was sucessful or failed
+    if amp_request.status_code == 201:
+        print('- Successfully added to SCD')
+    else:
+        print('- Something went wrong!')
